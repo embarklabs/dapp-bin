6e74d5114ed5ddc75805f49113cef7d9f557b5aa
Iuri Matias <iuri.matias@gmail.com>
2018-06-18 12:56:39 -0400

    update embark_demo

---
+++ embark_demo/config/contracts.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // default applies to all enviroments
+  default: {
+    // rpc to deploy the contracts
+    deployment: {
+      host: "localhost",
+      port: 8545,
+      type: "rpc"
+    },
+    // order of connections the dapp should connect to
+    dappConnection: [
+      "$WEB3",  // uses pre existing web3 object if available (e.g in Mist)
+      "ws://localhost:8546",
+      "http://localhost:8545"
+    ],
+    gas: "auto",
+    contracts: {
+      SimpleStorage: {
+        fromIndex: 0,
+        args: [ 100 ]
+      }
+    }
+  }
+}
